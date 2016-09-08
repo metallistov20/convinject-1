e93680e57b7e8376f80720b331733f0823e887bf
root <root@ubuntu.(none)>
2016-09-08 03:16:15 -0700

    All except LIBXML2

--- makeoff.cmd
+++ makeoff.cmd
@@ -1,6 +1,3 @@
-rm -frv ./_openssl
-mkdir --verbose ./_openssl
-
 rm -frv ./include/libcurl
 mkdir -p --verbose ./include/libcurl
 	
@@ -10,8 +7,15 @@ mkdir -p --verbose ./include/libssh
 rm -fr ./shared
 mkdir --verbose ./shared
 
-rm -fr ./_openssh
-mkdir --verbose ./_openssh
+
+
+
+rm -frv ./_openssl
+mkdir --verbose ./_openssl
+
+rm -frv ./_libssh
+mkdir -p --verbose ./_libssh
+
 
 #BEWARE:MAY_BE_UNSTABLE_THUS_MAY_DAMAGE_ENTIRE_PROCESS::git clone https://github.com/curl/curl.git
 wget    --no-check-certificate    https://curl.haxx.se/download/curl-7.50.2.tar.gz
@@ -37,13 +41,13 @@ cp --verbose ./include/curl/typecheck-gcc.h ../include/libcurl
 cd ..
 
 
-
+#BEWARE:DONT_DEL_OPENSSL_PRODUCT_UNTILL_DEPLOYED_LATER
 wget  https://www.openssl.org/source/old/1.0.0/openssl-1.0.0k.tar.gz
 tar xzvf openssl-1.0.0k.tar.gz
 rm -fr ./_openssl
 mv ./openssl-1.0.0k ./_openssl
 rm -fr ./openssl-1.0.0k
-#rm openssl-1.0.0k.tar.gz
+rm openssl-1.0.0k.tar.gz
 
 cd ./_openssl
 ./config --openssldir=/_product
@@ -51,5 +55,31 @@ ZT_CURRDIR=$(pwd) && make INSTALL_PREFIX=$ZT_CURRDIR install
 cd ..
 
 
-#rm -fr  ./_openssh
+
+# Don't del _openssl/_product untill deployed !
+
+
+git clone git://git.libssh.org/projects/libssh.git _libssh.src
+cd _libssh
+ZS_CURRDIR=$(pwd) && cmake -DCMAKE_INSTALL_DIR=$ZS_CURRDIR -DCMAKE_BUILD_TYPE=Debug ../_libssh.src \
+	-DOPENSSL_CRYPTO_LIBRARY=$ZS_CURRDIR/../_openssl/_product/lib/libcrypto.a \
+	-DOPENSSL_INCLUDE_DIR=$ZS_CURRDIR/../_openssl/_product/include \
+	-DOPENSSL_SSL_LIBRARY=$ZS_CURRDIR/../_openssl/_product/lib/libssl.a
+make
+cp --verbose ./src/libssh.so* ../shared
+cp  ../_libssh.src/include/libssh/callbacks.h ../include/libssh
+cp  ../_libssh.src/include/libssh/legacy.h ../include/libssh
+cp  ../_libssh.src/include/libssh/libssh.h ../include/libssh
+cp  ../_libssh.src/include/libssh/sftp.h ../include/libssh
+
+cd ..
+
+
+
+
+
+rm -frv ./_openssl
 rm -frv ./_libcurl
+rm -frv ./_libssh
+rm -frv ./_libssh.src
+
